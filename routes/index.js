61e2b0626979687df64ebadaf58fac86b12e8174
Varsha Panchal <varsha3051@gmail.com>
2022-02-12 17:27:36 -0500

    add index file for routes

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router  = express.Router();
+
+
+module.exports = (db) => {
+// home
+router.get("/", (req, res) => {
+  console.log('This is home');
+  res.render("index");
+});
+
+//login
+router.get("/login", (req, res) => {
+  console.log('loginnnnnnn');
+  res.render("index");
+});
+
+// router.post("/login", (req, res) => {
+//     res.status(200)
+//     console.log('login post');
+//     res.render("login");
+// });
+
+//register
+router.get("/register", (req, res) => {
+  res.render("index");
+  console.log('you registered successfully');
+});
+
+return router;
+}
