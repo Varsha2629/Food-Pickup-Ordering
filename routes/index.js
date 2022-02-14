8d8190966cb91e11e4d84ba04aa61135309ef7c0
Youssef Ragab <ragab.youssef95@gmail.com>
2022-02-14 18:53:34 -0500

    menu-page bug fixed

--- routes/index.js
+++ routes/index.js
@@ -35,17 +35,6 @@ router.get("/login", (req, res) => {
 //     res.render("login");
 // });
 
-<<<<<<< HEAD
-=======
-router.get("/menu", (req, res) => {
-  const menuItems = database.getAllMenuItems()
-  const templateVars = {
-    itmes : database.getAllMenuItems()
-  }
-  res.render('menu', database.getAllMenuItems());
-  // console.log(database.getAllMenuItems())
-});
->>>>>>> f5c61645e5606aa39c7e00e6576c4cf88eacd5b5
 
 return router;
 }
