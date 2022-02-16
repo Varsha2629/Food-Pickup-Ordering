a17207cedd382d33668b8854d98602bb587bcbf0
Zeinab Mohamed <zeinabtmohamed@gmail.com>
2022-02-16 18:43:27 +0000

    sms

--- server.js
+++ server.js
@@ -33,24 +33,20 @@ app.use(
 
 app.use(express.static("public"));
 
-// Separated Routes for each Resource
-// Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+  const login = require("./routes/index")
+  const homeRoutes = require("./routes/homepage")
+  const checkout = require("./routes/checkoutOrdersList")
 
-// Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
-// Note: mount other resources here, using the same pattern above
 
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
+  // login routes
+  app.use("/", login(db));
+  app.use("/menu", homeRoutes(db));
+  app.use("/checkout", checkout(db));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+
+
+  // Note: mount other resources here, using the same pattern above
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
