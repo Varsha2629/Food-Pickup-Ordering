4a219892b1fd06470c89f95ddae36606cca2d122
Zeinab Mohamed <zeinabtmohamed@gmail.com>
2022-02-16 18:00:22 +0000

    add order pick up message

--- sms.js
+++ sms.js
@@ -4,6 +4,8 @@ const client = require('twilio')(accountSid, authToken);
 
 client.messages
       .create({
+         body: 'Your order is ready for pick-up!',
+         messagingServiceSid: 'MGdcbeab9417496c80248a029666e20b2f',
          to: '+14164204433'
        })
       .then(message => console.log(message.sid))
